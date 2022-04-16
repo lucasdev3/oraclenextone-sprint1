2c828e0b4f5bfeedfe0c791a20dcc488cb025467
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-04-16 14:19:58 -0300

    arrumando footer e adicionando a limpeza do input após clique no botão

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,32 +4,27 @@ let buttonDescriptografia = document.getElementById('descriptografia');
 let botaoCopia = document.getElementById('copia');
 let arrayVogais = ['a', 'e', 'i', 'o', 'u'];
 let arrayVogalCriptografada = ['ai', 'enter', 'imes', 'ober', 'ufat'];
-let arrayLetrasPermitidas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+let arrayLetrasPermitidas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 var globalCriptografado = '';
 
 buttonCriptografia.addEventListener('click', () => {
     let frase = $('#frase').val();
-    
     let checkCaracteres = true;
-
-    for(let i of frase) {
-        if(arrayLetrasPermitidas.includes(i) === false) {
+    for (let i of frase) {
+        if (arrayLetrasPermitidas.includes(i) === false) {
             alert("Informe uma frase válida!");
             alert("A frase deve estar em minusculo e não pode conter numeros ou acentuação");
             checkCaracteres = false;
-            console.log(arrayLetrasPermitidas)
+
             return 0;
         }
     }
-
     if (!frase || !(frase.toLowerCase() === frase) || !checkCaracteres) {
         alert("Informe uma frase válida!");
         alert("A frase deve estar em minusculo e não pode conter numeros ou acentuação");
         return 0;
-    } 
-    
-    else {
-        console.log(frase);
+    }else {
+
         let fraseSplit = frase.split('');
         for (let i = 0; i < frase.length; i++) {
             for (let j = 0; j < arrayVogais.length; j++) {
@@ -45,29 +40,26 @@ buttonCriptografia.addEventListener('click', () => {
         titulo.appendChild(texto);
         elementoPai.appendChild(titulo);
         globalCriptografado = fraseCriptografada;
+        $('#frase').val('');
     }
 });
 
 buttonDescriptografia.addEventListener('click', () => {
     let frase = $('#frase').val();
     let checkCaracteres = true;
-
-    for(let i of frase) {
-        if(arrayLetrasPermitidas.includes(i) === false) {
-            console.log("entrou")
+    for (let i of frase) {
+        if (arrayLetrasPermitidas.includes(i) === false) {
             alert("Informe uma frase válida!");
             alert("A frase deve estar em minusculo e não pode conter numeros ou acentuação");
             checkCaracteres = false;
-            console.log(arrayLetrasPermitidas)
             return 0;
         }
     }
-    
+
     if (!frase || !(frase.toLowerCase() === frase) || !checkCaracteres) {
         alert("Informe uma frase válida!")
         alert("A frase deve estar em minusculo e não pode conter numeros ou acentuação");
     } else {
-        console.log(frase);
         var fraseDescriptografada = frase;
         let arrayTextReplace = [/ai/gi, /enter/gi, /imes/gi, /ober/gi, /ufat/gi];
         for (let i = 0; i < arrayTextReplace.length; i++) {
@@ -79,18 +71,17 @@ buttonDescriptografia.addEventListener('click', () => {
         let texto = document.createTextNode(`Frase à ser Descriptografada "${frase}": ${fraseDescriptografada}`);
         titulo.appendChild(texto);
         elementoPai.appendChild(titulo);
-        console.log(frase);
+        $('#frase').val('');
     }
 });
 
 botaoCopia.addEventListener('click', () => {
     // let frase = document.getElementById('frase');
     // frase.select();
-    if(globalCriptografado) {
+    if (globalCriptografado) {
         navigator.clipboard.writeText(globalCriptografado);
-        alert("Texto: " + globalCriptografado);
+        alert("Texto criptografado: " + globalCriptografado);
     } else {
         alert("Primeiro criptografe uma palavra ou texto");
     }
-    
-});nde
+});
